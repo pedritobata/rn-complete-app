ffa11b91050f38de4873bfaf10b3f11f2ad8f8c9
Pedro Martinez <pedromartinez@MacBook-Pro-de-Pedro.local>
2019-09-20 11:51:57 -0500

    flex example implementation

--- App.js
+++ App.js
@@ -1,25 +1,69 @@
-import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button , ScrollView} from 'react-native';
+import FlexExample from './flex-example';
 
 export default function App() {
+
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler = enteredText => {
+    setEnteredGoal(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    //console.log(enteredGoal);
+    //recordar que los tipos setState pueden recibir  (prevState, props)
+    setCourseGoals(currentGoals => [...currentGoals,enteredGoal]);
+  };
+
+
   return (
-     /* RN usa Flexbox por default!!. Ademas la orientacion del flex es column por default */
-    <View style={{padding: 50}}>
-      <View style={{flexDirection:'row', justifyContent:'space-between', alignItems:'center'}}>
+    /* RN usa Flexbox por default!!. Ademas la orientacion del flex es column por default */
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
         {/* En RN ningun texto puede ir fuera de un componente de texto */}
-        <TextInput 
-           placeholder="Course Goal"
-           style={{width:'80%' ,borderColor:'blue', borderWidth:1, padding:10}}
+        <TextInput
+          placeholder="Course Goal"
+          style={styles.input}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
         />
-        <Button title="ADD" />
+        <Button title="ADD" onPress={addGoalHandler}/>
       </View>
-      <View>
-  
-      </View>
+
+      {/* si quiero que el scroll esté disponible tengo que usar un ScrollView, el cual
+      tiene muchas propiedades */}
+      <ScrollView>{courseGoals.map(goal => 
+              /* tengo que envolver a Text dentro de un View ya que Text no acepta casi ningun style */
+               <View key={goal} style={styles.listItem}>
+                 <Text >{goal}</Text>
+               </View>
+               )}
+      </ScrollView>
+
+      <FlexExample ></FlexExample>
+
     </View>
   );
 }
 
+//tambien se puede crear un style con un objeto directo normal como en React.js
+//pero usando StyleSheet se gana en rendimiento y además me valida si los parametros
+//que estoy definiendo estan correctamente nombrados antes de deployar y que se caiga todo!!
 const styles = StyleSheet.create({
-  
+  screen: { padding: 50 },
+  inputContainer: { 
+    flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' 
+  },
+  input: { 
+    width: '80%', borderColor: 'blue', borderWidth: 1, padding: 10 
+  },
+  listItem: {
+    padding: 10,
+    marginVertical: 10,/** esta propiedad no existe en css */
+    backgroundColor: '#ccc',
+    borderWidth: 1,
+    borderColor: 'black'
+  }
 });
---
+++ flex-example.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+class FlexExample extends React.Component{
+
+  render(){
+      return (
+          /* no se pone display:flex porque RN es flex por default */
+          <View style={{
+              padding: 50,
+              width: "80%",
+              height: 300,
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center" /* esta propiedad es stretch por default, en cuyo caso los componentes
+                afectados se alrgarian verticalmente hasta los 300 del height definido */
+          }}>
+              <View style={{
+                  backgroundColor: 'red',
+                  /* la propiedad flex es la que comanda el espacio en el main axis */  
+                  flex:1,                  
+
+                  justifyContent: 'center',
+                  alignItems: 'center'
+              }}>
+                  
+                  <Text>1</Text>
+              </View>
+              <View style={{
+                  backgroundColor: 'blue',
+                  flex:2,
+                  alignItems: 'center',
+                  justifyContent: 'center'  
+              }}>
+                  <Text>2</Text>
+              </View>
+              <View style={{
+                  backgroundColor: 'green',
+
+                  alignItems: 'center',
+                  justifyContent: 'center'
+              }}>
+                  <Text>3</Text>
+              </View>
+          </View>
+      );
+  }
+
+}
+
+export default FlexExample;
